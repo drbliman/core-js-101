405259c728f3961d37711e23a437ecbc4c34fd82
drbliman <dr.bliman505@gmail.com>
2023-09-05 11:53:52 +0400

    feat: added solution part 2

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -127,8 +127,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const a = String(value);
+  return Number(a[a.length - 1]);
 }
 
 
@@ -143,8 +144,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -160,8 +161,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return (a ** 2 + b ** 2 + c ** 2) ** (1 / 2);
 }
 
 
@@ -182,8 +183,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const a = 1 * (10 ** pow);
+  let num1 = Math.round(num / a);
+  num1 *= a;
+  return num1;
 }
 
 /**
@@ -203,8 +207,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let sum = 0;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      sum += 1;
+    }
+  }
+  if (n === 2) {
+    return true;
+  }
+  return sum === 0;
 }
 
 /**
@@ -222,8 +235,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return +value || def;
 }
 
 module.exports = {
